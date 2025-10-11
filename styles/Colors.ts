d8bb8141cbf86e2a9333ae1851a546d4ba0bf63f
hortuadev <hortuadev@gmail.com>
2025-10-11 08:34:31 +0100

    style(Colors): agregada la paleta de colores del proyecto

---
+++ styles/Colors.ts
@@ -0,0 +1,14 @@
+export const Color = {
+    // Content 
+    primaryColor : "#1f2c34",
+    secondaryColor: "#121b22",
+    // buttons
+    buttonPrimaryColor:"#0e3031",
+    buttonSecondaryColor:"#02a785",
+    buttonThirdColor:"#3c4a55",
+    // text
+    textPrimaryColor:"#86959e",
+    textSecondaryColor:"#ecf0f1",
+    textThirdColor:"#012623",
+
+};
