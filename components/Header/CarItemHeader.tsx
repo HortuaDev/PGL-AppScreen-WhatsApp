ee9c387fb3fd2c8e6d5f9cac68c6026d9b2ccae8
hortuadev <hortuadev@gmail.com>
2025-10-11 09:11:14 +0100

    feat(CardItemHeader): creado el componente carItemHeader y el archivo de los estilos

---
+++ components/Header/CarItemHeader.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CarItemHeader = () => {
+  return (
+    <View>
+      <Text>CarItemHeader</Text>
+    </View>
+  );
+};
+
+export default CarItemHeader;
+
+const styles = StyleSheet.create({});
--- components/Header/HeaderComponent.tsx
+++ components/Header/HeaderComponent.tsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Color } from "../../styles/Colors";
 
 const HeaderComponent = () => {
+  interface CardItems {
+    image: string;
+    text: string;
+    notifications: number;
+  }
+
   return (
-    <View>
-      <Text>HeaderComponent</Text>
+    <View style={styles.headerContainer}>
+      <View>
+        <View>
+          <Text></Text>
+        </View>
+        <View>
+          <Image></Image>
+          <Image></Image>
+        </View>
+        <ScrollView horizontal={true}></ScrollView>
+      </View>
     </View>
   );
 };
 
 export default HeaderComponent;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerContainer: { backgroundColor: Color.primaryColor },
+});
