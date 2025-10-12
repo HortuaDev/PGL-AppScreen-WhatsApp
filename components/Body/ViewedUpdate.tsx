1d6f6d903e73d2129d0f51561e0ea4c7f9fcd2ca
hortuadev <hortuadev@gmail.com>
2025-10-12 10:57:50 +0100

    fix(ViewdUpdate.tsx): creado y estilizado el componente viewdupdate

--- components/Body/ViewedUpdate.tsx
+++ components/Body/ViewedUpdate.tsx
@@ -1,15 +1,18 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Color } from "../../styles/Colors";
 
 const ViewedUpdate = () => {
   return (
     <View style={styles.viewUpdateContainer}>
       <View style={styles.TextContentViewUpdate}>
-        <Text style={styles.textTitleView}></Text>
+        <Text style={styles.textTitleView}>Viewed Updates</Text>
+        <Image
+          style={styles.iconView}
+          source={require("../../assets/images/icon_arrow_down_48.png")}
+        />
       </View>
-      <View style={styles.iconViewContainer}>
-        <Image style={styles.iconView} />
-      </View>
+
       <View style={styles.endViewUpdateLine}></View>
     </View>
   );
@@ -18,10 +21,24 @@ const ViewedUpdate = () => {
 export default ViewedUpdate;
 
 const styles = StyleSheet.create({
-  viewUpdateContainer: {},
-  TextContentViewUpdate: {},
-  textTitleView: {},
-  iconViewContainer: {},
-  iconView: {},
-  endViewUpdateLine: {},
+  viewUpdateContainer: {
+    flex: 1,
+    paddingHorizontal: 15,
+    backgroundColor: Color.secondaryColor,
+  },
+  TextContentViewUpdate: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  textTitleView: {
+    flex: 1,
+    color: Color.textPrimaryColor,
+    fontSize: 20,
+  },
+  iconView: { height: 25, width: 25 },
+  endViewUpdateLine: {
+    flex: 1,
+    height: 2,
+    backgroundColor: Color.textPrimaryColor,
+  },
 });
